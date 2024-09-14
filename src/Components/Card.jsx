ce86171a6d89ddaafaf2fca53e589f49ea117d50
V <160890362+vivek28Jaiswal@users.noreply.github.com>
2024-09-14 22:23:42 +0530

    conditional rendering

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 function Card(){
 
     let data = [
-        {image: 'https://images.unsplash.com/photo-1658842042844-eeb5ad17b7d3?q=80&w=1858&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', name: "Mindset.", description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. In eius ab necessitatibus consequuntur adipisci suscipit."},
-        {image: 'https://images.unsplash.com/photo-1663868652516-dabef3845d29?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', name: "Tom Hanks.", description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. In eius ab necessitatibus consequuntur adipisci suscipit."},
-        {image: 'https://images.unsplash.com/photo-1624979393459-37e156d6ebce?q=80&w=1958&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', name: "Wanted.", description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. In eius ab necessitatibus consequuntur adipisci suscipit."},
+        {image: 'https://images.unsplash.com/photo-1658842042844-eeb5ad17b7d3?q=80&w=1858&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', name: "Mindset.", description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. In eius ab necessitatibus consequuntur adipisci suscipit.", instock: false},
+        {image: 'https://images.unsplash.com/photo-1663868652516-dabef3845d29?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', name: "Tom Hanks.", description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. In eius ab necessitatibus consequuntur adipisci suscipit.", insstock: false},
+        {image: 'https://images.unsplash.com/photo-1624979393459-37e156d6ebce?q=80&w=1958&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', name: "Wanted.", description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. In eius ab necessitatibus consequuntur adipisci suscipit.", instock: true},
     ];
 
     return(
@@ -18,6 +18,9 @@ function Card(){
             <div className='px-4 py-4'>
                 <h2 className='mt-4 text-lg font-semibold'>{elem.name}</h2>
                 <p className='mt-2 text-xs'>{elem.description}</p>
+                <button className={`px-4 py-1 text-white mt-4 ${elem.instock ? "bg-blue-600" : "bg-red-600"} rounded text-xs`}>
+                    {elem.instock ? "In Stock" : "Out Of Stock"}
+                </button>
             </div>
             </div>
             ))}
