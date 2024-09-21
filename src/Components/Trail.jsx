c63e84f39ced8f53085685442a7ce42771dd6d35
V <160890362+vivek28Jaiswal@users.noreply.github.com>
2024-09-21 18:16:21 +0530

    img btn event

--- src/Components/Trail.jsx
+++ src/Components/Trail.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FaLongArrowAltRight } from "react-icons/fa";
-
 const Trail = () => {
+  const [val, setVal]= useState(false);
   return (
-    <div className='w-full h-screen bg-zinc-700 flex items-center justify-center'>
-        <div className='relative h-56 w-96 bg-red-200 rounded-md flex overflow-hidden'>
-            <img className='shrink-0 w-full h-full object-cover' src="https://media.istockphoto.com/id/515816334/photo/christ-church-shimla.webp?a=1&b=1&s=612x612&w=0&k=20&c=npIR4bpq94pRFepmuTCfNY0W6YiiP6orurln839crP8=" alt="" />
-            <img className='shrink-0 w-full h-full object-cover' src="https://images.unsplash.com/photo-1718712760956-3e527953827b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
-            <span className='w-14 h-14 absolute rounded-full flex items-center justify-center left-1/2 bottom-[1%] -translate-x-[50%] -translate-y-[50%] bg-[#dadada8b]'>
-            <FaLongArrowAltRight size={"1.5em"} />
+    <div className='w-full h-screen bg-neutral-400 flex items-center justify-center'>
+        <div className='relative h-56 w-96 rounded-md flex overflow-hidden'>
+            <img className={`duration-700 ease-in-out	 shrink-0 ${val === false ? "-translate-x-[0]" : "-translate-x-[100%]"} w-full h-full object-cover`} src="https://images.unsplash.com/photo-1726487646639-ec039193792f?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+            <img className={`duration-300 ease-in-out	 shrink-0 ${val === false ? "-translate-x-[0]" : "-translate-x-[100%]"} w-full h-full object-cover`} src="https://images.unsplash.com/photo-1726758254279-6a39c11bdcd5?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+            <span onClick={()=> setVal(()=> !val)} className={`duration-700 ease-in-out w-12 h-12 absolute rounded-full flex items-center justify-center left-1/2 bottom-[1%] -translate-x-[50%] -translate-y-[50%] bg-[#333] ${val === false ? "rotate-0" : "-rotate-180"}`}>
+            <FaLongArrowAltRight size={"1.4em"} color='white' opacity={".9"} />
             </span>
         </div>
     </div>
