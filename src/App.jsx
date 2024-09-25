e16abea1114b77a026bc81b80618fc997f727c83
V <160890362+vivek28Jaiswal@users.noreply.github.com>
2024-09-25 14:02:27 +0530

    use of props and useState

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,16 @@ import Trail from './Components/Trail';
 import Button from './Components/Button';
 
 function App(){
-
+    let data = [
+        {name: 'yami', profession: 'actross', salary: 120000, img: "https://plus.unsplash.com/premium_photo-1669951867704-a78fd21fbbd2?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
+        {name: 'Riwick', profession: 'coder', salary: 75000, img: "https://images.unsplash.com/photo-1528892952291-009c663ce843?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
+        {name: 'Jhonathan', profession: 'gamer', salary: 65000, img: "https://images.unsplash.com/photo-1517012021249-a130aa959065?q=80&w=2009&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
+    ]
     return(
-        <div>
-            {/* <Card/> */}
-            {/* <Practice/> */}
-            {/* <Trail/> */}
-            <Button text="Know more" color="bg-blue-500"/>
-            <Button text="Downlaod" color="bg-red-500"/> 
+        <div className='w-full h-screen bg-zinc-400 flex items-center justify-center gap-4'>
+            {data.map((item, index)=>(
+                <Card name={item.name} profession={item.profession} img={item.img}/>
+            ))}
         </div>
     );
 }
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,20 +1,20 @@
-import React, { useState } from 'react';
+import React from 'react'
 
-function Card(){
-    const [val, setVal] = useState(0);
-    const newVal = () => setVal((prev)=> prev+1);
-    const prevVal = () => setVal((prev)=> prev-1);
-    return(
-        <div className='w-full h-screen items-center flex justify-center flex-col'>
-        <h2 className='text-xl font-semibold'>{val}</h2>
-        <button onClick={newVal} className='px-4 py-1 mt-4 text-xs font-semibold bg-green-500 rounded-full'>
-            start
-        </button>
-        <button onClick={prevVal} className='px-4 py-1 mt-4 text-xs font-semibold bg-green-500 rounded-full'>
-            Prev
-        </button>
+const Card = ({name, profession, img}) => {
+  return (
+    <div className='w-64 bg-zinc-200 rounded-md'>
+        <div className='w-full h-48'>
+            <img className='w-full h-full object-cover object-center rounded-t-md' src={img} alt="" />
         </div>
-    );
+        <div className='w-full p-2'>
+        <h4 className='text-sm font-semibold capitalize'>{name}</h4>
+        <h5 className='text-xs capitalize'>{profession}</h5>
+        <div>
+        <button className='px-4 py-1 bg-blue-500 text-white text-xs mt-2 rounded-md'>Connect</button>
+        </div>
+        </div>
+    </div>
+  )
 }
 
-export default Card;
+export default Card
