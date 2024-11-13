1ccecc744a64cd95f9c78d27d3fad8045f94c120
V <160890362+vivek28Jaiswal@users.noreply.github.com>
2024-11-13 19:06:52 +0530

    songs cardss

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import styles from "./style.module.css";
 
 function Navbar({data}) {
   return (
     <div className='w-full flex justify-between items-center py-6 px-20 text-black text-xl'>
-        <h2>LisMelo</h2>
+        <h2 className={styles.changeColor} >LisMelo</h2>
         <div className='flex justify-between items-center rounded-md py-2 px-5 text-sm font-semibold text-black bg-[#22C55E] gap-2'>
             <h4>Most loved</h4>
             <h5>{data.filter(item => item.added).length}</h5>
